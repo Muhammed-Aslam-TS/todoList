8d6fc30d38775fd49bea651ea705f12632d1e333
muhammed aslam <taslam8797@gmail.com>
2023-03-16 17:17:39 +0530

    todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
+import { TodoformComponent } from './todoform/todoform.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoformComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
